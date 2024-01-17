ae60e180a3bc2ab75db1a6c1d7e7e6bd76833085
hunterstorm <huntersreese@gmail.com>
2024-01-17 13:47:39 -0800

    dynamic position data JSON parsing

--- back-end/src/routes/Rscripts/league-stats/PositionData.js
+++ back-end/src/routes/Rscripts/league-stats/PositionData.js
@@ -7,24 +7,26 @@ const { execSync } = require('child_process');
 const scriptDirectory = path.join(__dirname, '../../../../../scripts');
 process.chdir(scriptDirectory);
 
-const positions = [ 'qb_leaders_2023', 'wr_leaders_2023', 'rb_leaders_2023'];
+const positions = [ 'qb_leaders_2023', 'wr_leaders_2023', 'rb_leaders_2023', 'te_leaders_2023', 'k_leaders_2023'];
+
+const commands = ['Rscript position_data.R', 'Rscript kicking_data.R']
 
 router.get('/', async (req, res) => {
     try {
-        const command = 'Rscript position_data.R'
-        const result = execSync(command, {encoding: 'utf-8'})
-
-        const jsonData = {};
-
+        //run scripts
+        for (const command of commands) {
+            execSync(command, {encoding: 'utf-8'})
+        }
+        
         // Load JSON data from files dynamically
+        const jsonData = {};
         for (const position of positions) {
             const filePath = `json-files/position-data/${position}.json`;
             const fileContent = await fs.readFile(filePath, 'utf-8');
             jsonData[position] = JSON.parse(fileContent);
         }
-
-        console.log('Parsed JSON Data:', jsonData);
         res.json(jsonData);
+
     } catch (error) {
         console.error('Error running script', error);
         console.error('Script stderr:', error.stderr.toString());
