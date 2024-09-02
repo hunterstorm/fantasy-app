3f5fa2a729a177468bc2e93220823d57f15c3ada
evtrey <etreynolds31@gmail.com>
2024-09-01 21:34:38 -0400

    team_stats script added!

--- scripts/team_stats.R
+++ scripts/team_stats.R
@@ -4,17 +4,32 @@ dependent <- TRUE
 szn <- 2023
 source("config/script_config.R", local = TRUE)
 
+# Calculate team offensive stats using pbp
+
+team_stats <- pbp %>%
+  group_by(posteam) %>%
+  summarize(
+    total_yards = sum(yards_gained, na.rm = TRUE),
+    total_run_yards = sum(yards_gained[play_type == "run"], na.rm = TRUE),
+    total_pass_yards = sum(yards_gained[play_type == "pass"], na.rm = TRUE),
+    total_fg_made = sum(play_type == "field_goal" & field_goal_result == "made", na.rm = TRUE),
+    total_fg_missed = sum(play_type == "field_goal" & field_goal_result == "missed", na.rm = TRUE),
+    fg_rate = total_fg_made / sum(play_type == "field_goal", na.rm = TRUE)
+  )
+
+# Calculate run and pass rate, then join to team_stats
+
 # clean up pbp data to count run and pass plays by team
 off_run_pass <- pbp %>%
   filter(play_type == "run" | play_type == "pass") %>%
   select(posteam, play_type) %>%
   group_by(posteam, play_type) %>%
-  summarise(count = n())
+  summarize(count = n())
 
 # calculate total run and pass plays per team
 off_total_plays <- off_run_pass %>%
   group_by(posteam) %>%
-  summarise(total_plays = sum(count))
+  summarize(total_plays = sum(count))
 
 # use total_plays to calculate individual run and pass rates per team
 off_run_pass_ratio <- off_run_pass %>%
@@ -25,4 +40,8 @@ off_run_pass_ratio <- off_run_pass %>%
   pivot_wider(names_from = play_type, values_from = ratio)
 
 # renaming columns to something more readable
-colnames(off_run_pass_ratio) <- c("posteam","run_rate","pass_rate")
+colnames(off_run_pass_ratio) <- c("posteam", "run_rate", "pass_rate")
+
+# join rate data to team_stats
+team_stats <- off_run_pass_ratio %>%
+  full_join(team_stats, by = "posteam")
